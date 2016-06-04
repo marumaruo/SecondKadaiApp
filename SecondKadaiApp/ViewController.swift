8d8c5af26dc9574b18dc19fd9a3eb031039f53ca
bc0067042 <bc0067042@ishi-pc20192992.local>
2016-06-04 23:24:16 +0900

    update

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func unwind(segue: UIStoryboardSegue) {
     }
 
+    @IBOutlet weak var textField: UITextField!
+
+    @IBOutlet weak var textFieldAndButtonMargine: NSLayoutConstraint!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additiona2l setup after loading the view, typically from a nib.
+        
+        textFieldAndButtonMargine.constant = CGFloat(0)
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -24,12 +38,7 @@ class ViewController: UIViewController {
         let secondViewController:SecondViewController = segue.destinationViewController as! SecondViewController
         secondViewController.text = textField.text
     }
-
-    @IBAction func unwind(segue: UIStoryboardSegue) {
-        
-    }
-
-    @IBOutlet weak var textField: UITextField!
+    
     
     
 }
